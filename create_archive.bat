0e6ea30670b70cb91bbe3b18de7bed22f33b6629
Jocelyn Fiat <github@djoce.net>
2009-08-28 10:33:37 +0200

    added small  doc script to create quick archive

---
+++ create_archive.bat
@@ -0,0 +1,2 @@
+del gobo-safe.7z
+7z a -r -x!*.exe -x!*.html -x!*.c -x!*.txt -x!*.bat -x!.svn -x!svn\bin -x!svn\doc -x!svn\misc -x!svn\test -x!svn\work -x!svn\tool -x!svn\example  gobo-safe.7z src override svn *.ecf
